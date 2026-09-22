28bd405b801cd783b71b1d4d48243c5ae5521444
Kanishka Chaudhary <kanishkachaudhary.vr@gmail.com>
2026-09-22 23:42:10 +0530

    Bump compileSdk to 35 for Compose compatibility

--- android/app/src/main/java/com/finsentinel/app/NotificationListener.kt
+++ android/app/src/main/java/com/finsentinel/app/NotificationListener.kt
@@ -28,7 +28,7 @@ class NotificationListener : NotificationListenerService() {
 
     // CONFIGURABLE BACKEND URL
     // Change this to your local IP (e.g. http://192.168.1.5:8000) or your production domain
-    private val BACKEND_URL = "http://192.168.1.100:8000"
+    private val BACKEND_URL = "http://192.168.1.3:8000"
     
     private val scope = CoroutineScope(Dispatchers.IO)
     private val jsonMediaType = "application/json; charset=utf-8".toMediaType()
