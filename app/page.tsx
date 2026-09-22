330f1efbc79e3a536260522790dbb2812cef94f2
Kanishka Chaudhary <kanishkachaudhary.vr@gmail.com>
2026-09-23 00:39:48 +0530

    Bypass Next.js auth database check for local hackathon demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import { headers } from 'next/headers'
-import { redirect } from 'next/navigation'
 import { FinSentinelDashboard } from '@/components/fin-sentinel-dashboard'
-import { auth } from '@/lib/auth'
 
-export default async function Home() {
-  const session = await auth.api.getSession({ headers: await headers() })
-  if (!session?.user) redirect('/sign-in')
-  return <FinSentinelDashboard userName={session.user.name || 'there'} />
+export default function Home() {
+  return <FinSentinelDashboard userName="Kanishka" />
 }
